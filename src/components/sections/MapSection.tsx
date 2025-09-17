7ba26aef5557654f1300f1c0b8ec63d26ab5269c
[dyad] <upetif@gmail.com>
2025-09-18 00:43:28 +0200

    Updated src/components/sections/MapSection.tsx

--- src/components/sections/MapSection.tsx
+++ src/components/sections/MapSection.tsx
@@ -75,7 +75,7 @@ const HungaryMap = () => {
 
 const MapSection = () => {
   return (
-    <section id="map" className="py-20 px-6 bg-black/30">
+    <section id="map" className="py-20 px-6">
       <div className="container mx-auto text-center">
         <Badge className="mb-4 bg-gradient-to-r from-cyan-500 to-purple-500 text-white border-0">
           <MapPin className="h-4 w-4 mr-2" />
