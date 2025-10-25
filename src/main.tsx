367bee44d9f2ef3d51806b9a5042f4da58c3ccea
[dyad] <upetif@gmail.com>
2025-10-25 12:54:14 +0200

    Reverted all changes back to version f252f31c0eebf39912e247f24f5070853105dab8

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import React from 'react';
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 
-const root = document.getElementById('root');
-
-if (root) {
-  createRoot(root).render(<App />);
-}
+createRoot(document.getElementById("root")!).render(<App />);
